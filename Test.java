516993ce8884ba70ae70af16be6bb9e5bdaa724e
Yudai <yudaimadeinjapan@gmail.com>
2022-10-27 15:49:13 +0100

    Added variables, constructor, displayCollection

---
+++ Test.java
@@ -0,0 +1,36 @@
+public class Test implements CollectionTest {
+
+    CollectionType collectionType;
+    TestType testType;
+
+
+    public Test(){
+    
+    }
+
+    @Override
+    public void setSize(int size) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void runTest(CollectionTest.CollectionType type, CollectionTest.TestType test, int iterations) {
+        
+        switch(testType){
+            case ADD:
+
+            case INDEX:
+            
+            case SEARCH:
+        }
+     
+    }
+
+    public void displayCollection(){
+        
+    }
+
+
+    
+}
