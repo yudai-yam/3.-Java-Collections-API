d0da0315951138db6f62ef8e6dc4182e95948e9f
Yudai <yudaimadeinjapan@gmail.com>
2022-11-09 02:20:58 +0000

    for some reason if you use .get it starts saying like cant find a constructor and i still haven't figured out how to implement SEARCH for HASH_MAP

--- Tester.java
+++ Tester.java
@@ -5,18 +5,16 @@ import java.util.Random;
 
 public class Tester implements CollectionTest{
 
-    private CollectionType collectionType;
-    private TestType testType;
     private int size;
 
     @Override
     public void setSize(int size) {
-        // TODO Auto-generated method stub
-        
+        this.size = size;   
     }
 
+
     @Override
-    public void runTest(CollectionTest.CollectionType type, CollectionTest.TestType test, int iterations) {
+    public void runTest(CollectionType type, TestType test, int iterations) {
         for (int i=0; i<iterations; i++){
 
             LinkedList<Person> list = new LinkedList<Person>();
@@ -32,7 +30,7 @@ public class Tester implements CollectionTest{
                                 Random random = new Random();
                                 list.add(new Person("Person" + k, random.nextInt(100)));
                             }
-                            System.out.println(list);
+                    
                         break;
 
                         case ARRAY_LIST:                          
@@ -41,7 +39,6 @@ public class Tester implements CollectionTest{
                                 Random random = new Random();
                                 al.add(new Person("Person" + k, random.nextInt(100)));
                             }
-                            System.out.println(al);
                         break;
 
                         case HASH_MAP:
@@ -50,60 +47,55 @@ public class Tester implements CollectionTest{
                                 Random random = new Random();
                                 hash_map.put(k, new Person("Person" + k, random.nextInt(100)));
                             }
-                            System.out.println(al);
                             
                         break;
                     }
                     break;
 
                 case INDEX:
-                    switch(collectionType){
+                    switch(type){
                         case LINKED_LIST:
-                            int list_index = size/2;
-                            System.out.println("The person's name is: " + list.get(list_index).getName() + "and the age is: " + list.get(list_index).getAge());
+                            
                         break;
 
                         case ARRAY_LIST:
-                            int array_index = size/2;
-                            System.out.println("The person's name is: " + al.get(array_index).getName() + "and the age is: " + al.get(array_index).getAge());
+                            //int array_index = size/2;
+                            //System.out.println("The person's name is: " + al.get(array_index).getName() + "and the age is: " + al.get(array_index).getAge());
                         break;
 
                         case HASH_MAP:
-                            int hash_index = size/2;
-                            System.out.println("The person's name is: " + hash_map.get(hash_index).getName() + "and the age is: " + hash_map.get(hash_index).getAge());
+                            //int hash_index = size/2;
+                            //System.out.println("The person's name is: " + hash_map.get(hash_index).getName() + "and the age is: " + hash_map.get(hash_index).getAge());
                         break;
                     }
                     break;
 
                 case SEARCH:
-                    switch(collectionType){
+                    switch(type){
                         case LINKED_LIST:
                             for (Person list_person : list){
-                                if (list_person.getName().equals("Person5")){
-                                    System.out.println("Found! The age is "+list_person.getAge());
+                                if (list_person.getName().equals("Person10")){
+                                    break;
                                 }
                             }
                         break;
 
                         case ARRAY_LIST:
                             for(Person array_person : al) {
-                                if(array_person.getName().equals("Person5")) {
-                                    System.out.println("Found! The age is "+array_person.getAge());
+                                if(array_person.getName().equals("Person10")){
+                                    break;
                                 }
                             }
                         break;
 
-                        case HASH_MAP:
-                        /*for (hash_map hash_person : hash_map.entrySet()) {
-                            if (hash_person.getName().equals("Person5", hash_map.get)) {
-                                return hash_map.getKey();
-                            }
-                        }*/
+                        case HASH_MAP:          
+                            
+                            
                         break;
                     }
                     break;
             }
         }        
     }
-
 }
+
